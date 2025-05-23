acc9f06e41ce0d908ffe3a26b8a5e56d891709c7
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-23 20:01:40 +0500

    connected Header.vue and added some style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+</script>
 
 <template>
-
-
+  <div class="bg-[#FFFFFF] w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+    <Header />
+    <h1>Ava</h1>
+  </div>
 </template>
 
 <style></style>
