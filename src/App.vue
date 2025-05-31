f49622fdd7f483ae620e81c61eccc852cc26fed4
KhamdamovAva <Ava.lifeone@gmail.com>
2025-05-31 13:29:11 +0500

    added sort function and axios library

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,43 @@
 <script setup>
+import { onMounted, reactive, ref, watch } from 'vue'
 import Header from './components/Header.vue'
 import CardList from './components/CardList.vue'
 import Drawer from './components/Drawer.vue'
+import axios from 'axios'
+
+const items = ref([])
+
+const filters = reactive({
+  sortBy: '',
+  searchQuery: '',
+})
+
+const sortBy = ref('')
+const searchQuery = ref('')
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get('https://015389e9b582cf1a.mokky.dev/items')
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+})
+// Сортировка по ценам
+watch(filters, async () => {
+  try {
+    const { data } = await axios.get(
+      'https://015389e9b582cf1a.mokky.dev/items?sortBy=' + filters.sortBy,
+    )
+    items.value = data
+  } catch (error) {
+    console.log(error)
+  }
+})
 </script>
 
 <template>
@@ -13,10 +49,10 @@ import Drawer from './components/Drawer.vue'
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (дешевые)</option>
-            <option>По цене (дорогие)</option>
+          <select @change="onChangeSelect" class="py-2 px-3 border rounded-md outline-none">
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогие)</option>
           </select>
           <div class="relative">
             <img class="absolute left-4 top-3" src="/icons/search.svg" alt="search icon" />
@@ -27,7 +63,9 @@ import Drawer from './components/Drawer.vue'
           </div>
         </div>
       </div>
-      <CardList />
+      <div class="mt-10">
+        <CardList :items="items" />
+      </div>
     </div>
   </div>
 </template>
